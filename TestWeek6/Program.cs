0f9797a22bb929674449f31491a76695fd123b00
TanelP <tanel.pool@tktk.ee>
2024-12-20 15:53:04 +0200

    add week6 code

---
+++ TestWeek6/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string[] colors = { "red", "green", "blue", "pink", "yellow" };
+
+//for (int i = 0; < i colors.Length; int++ ){
+//    }
+
+
+foreach (string color in colors)
+{
+    colors = colors.ToArray();
+}
+
+Console.WriteLine(colors);
